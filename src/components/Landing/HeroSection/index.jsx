c0d6a4971f4e317a619d12931f00ad5253869885
METi <mahdik1385427@gmail.com>
2024-10-06 00:24:24 +0330

    HeroSection fix

--- src/components/Landing/HeroSection/index.jsx
+++ src/components/Landing/HeroSection/index.jsx
@@ -4,7 +4,7 @@ import Body from "./Body/Body";
 function Header() {
   return (
     <>
-      <div className="Header w-full h-[850px] mt-[-80px] flex justify-center flex-wrap pt-[120px] bg-[#E3F2FD] bg-no-repeat max-xl:h-[800px] max-short:h-[700px]">
+      <div className="Header w-full h-[850px] bg-[length:103%]  mt-[-80px] flex justify-center flex-wrap pt-[120px] bg-[#E3F2FD] bg-no-repeat max-xl:bg-[length:1500px] max-lg:bg-[length:1300px] max-xl:h-[800px] max-short:h-[700px]">
         <Body />
       </div>
     </>
