94f0d4618c345f69cc5783c9e615690b481def97
barane <baranemirzaei@gmail.com>
2024-10-08 00:16:55 +0330

    edit footer

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import HeroSection from "./HeroSection/index";
 import Servises from "./Servises/index";
-import Footer from "../../app/Layouts/MainLayout/Footer/index";
 const Landing = () => {
   return (
     <div>
       <HeroSection />
       <Servises />
-      <Footer />
     </div>
   );
 };
