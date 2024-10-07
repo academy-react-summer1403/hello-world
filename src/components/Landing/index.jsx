391915e4833648fb5028b51cc6e03732f4886e93
METi <mahdik1385427@gmail.com>
2024-10-07 23:59:12 +0330

    categoori

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from "../../app/Layouts/MainLayout/Navbar/Navbar"
 import HeroSection from "./HeroSection/index"
+import Categories from "./Categories/index"
 
 const Landing = () => {
   return (
     <div>
       <Navbar/>
       <HeroSection/>
+      <Categories/>
     </div>
   )
 }
