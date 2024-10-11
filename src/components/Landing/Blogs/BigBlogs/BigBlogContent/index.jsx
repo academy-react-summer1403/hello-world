353f413a160b2f397733ffcdef2bfc3513f7367f
barane <baranemirzaei@gmail.com>
2024-10-11 21:32:09 +0330

    create blog

---
+++ src/components/Landing/Blogs/BigBlogs/BigBlogContent/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BigBlogContent = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BigBlogContent;
--- src/components/Landing/Blogs/index.jsx
+++ src/components/Landing/Blogs/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import vector from "@assets/images/Landing/Servises/Vector 20.png"
 
 const Blogs = () => {
   return (
-    <div>
-      
-    </div>
+    <div className="w-full flex justify-center pt-20 ">
+    <div className="w-[1200px] h-[830px] flex justify-center flex-wrap ">
+      <div className="w-full h-20 text-center text-[#263238] text-4xl pt-2 myFontBold max-xl:mb-10">
+        <h2 className="w-full font-['YekanBakhBold'] ">اخبار و مقالات</h2>
+        <img className="m-auto mt-4" src={vector} alt="" />
+      </div>
+
+      </div>
+      </div>
   )
 }
 
