a45e4f7cc13574e1b2283d20f2cbb9309ee38241
METi <mahdik1385427@gmail.com>
2024-10-14 14:13:52 +0330

    fix

--- src/components/Landing/Categoris/index.jsx
+++ src/components/Landing/Categoris/index.jsx
@@ -35,108 +35,147 @@ const Categories = () => {
           </p>
         </div>
         <div className="item flex    w-[688px] h-[752px]  z-50   absolute  left-[80px] top-0  ">
-          <div className="it1  border-[#eeeeee] border-[1px] border-solid max-xl:w-[270px] max-xl:h-[270px]  max-lgx:w-[240px] max-lgx:h-[240px]  w-[300px] h-[300px] rounded-3xl   absolute  left-0 top-[80px] bg-[#ffffff]     ">
-            <div className="icon1  w-[80px]  h-[80px] absolute  top-[50px] inset-x-[110px] max-xl:left-24  max-lgx:left-20 max-lgx:top-[33px] ">
+          <div className="it1 flex justify-center items-center py-10 flex-wrap  border-[#eeeeee] border-[1px] border-solid max-xl:w-[270px] max-xl:h-[270px]  max-lgx:w-[240px] max-lgx:h-[240px]  w-[300px] h-[300px] rounded-3xl   absolute  left-0 top-[80px] bg-[#ffffff]     ">
+            <div className="icon1   w-[80px]  h-[80px]  max-lgx:top-[33px] ">
               <img src={pic6} alt="image" />{" "}
             </div>
-            <h1 className="text-gray3 text-center mt-36 text-[24px] font-[YekanBakhBold] leading-[50px]  max-lgx:text-[19px] max-lgx:mt-[120px]">
+            <h1 className="text-gray3 w-full  text-center text-[24px] font-[YekanBakhBold] leading-[50px]  max-lgx:text-[19px]">
               {" "}
               بازی
             </h1>
-            <p className="text-gray4  text-center w-[200px] text-[13px] font-[YekanBakh] ml-[50px] max-xl:ml-[40px] max-lgx:text-[11px] max-lgx:ml-[20px]">
+            <p className="text-gray4   text-center w-[200px] text-[13px] font-[YekanBakh] max-lgx:text-[11px] ">
               {" "}
-              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-              استفاده از طراحان گرافیک است{" "}
+              بعد از اتمام آموزش، به شرکت‌ها و سرمایه‌گذاران بازی‌ساز معرفی خواهی شد
             </p>
           </div>
 
-          <div className="it2  border-[#eeeeee] border-[1px] border-solid max-xl:w-[270px] max-xl:h-[270px]   max-lgx:w-[240px] max-lgx:h-[240px]  w-[300px] h-[300px] rounded-3xl   absolute max-xl:right-10 max-lgx:right-36 max-lgx:top-10 right-0 top-[10px]  bg-white   ">
-            <div className="icon2  w-[80px]  h-[80px] absolute  top-[50px] inset-x-[110px] max-xl:left-24  max-lgx:left-20 max-lgx:top-[33px]">
+          <div className="it2 flex justify-center items-center py-10 flex-wrap border-[#eeeeee] border-[1px] border-solid max-xl:w-[270px] max-xl:h-[270px]   max-lgx:w-[240px] max-lgx:h-[240px]  w-[300px] h-[300px] rounded-3xl   absolute max-xl:right-10 max-lgx:right-36 max-lgx:top-10 right-0 top-[10px]  bg-white   ">
+            <div className="icon2  w-[80px]  h-[80px] ">
               <img src={pic7} alt="image" />{" "}
             </div>
-            <h1 className="text-gray3 text-center text-3xl mt-36  text-[24px] font-[YekanBakhBold] leading-[50px]  max-lgx:text-[19px] max-lgx:mt-[120px]    ">
+            <h1 className="text-gray3 w-full text-center text-3xl text-[24px] font-[YekanBakhBold] leading-[50px]  max-lgx:text-[19px]    ">
               تحلیل داده
             </h1>
-            <p className="text-gray4  text-center  w-[200px] text-[13px] font-[YekanBakh] ml-[50px] max-xl:ml-[40px] max-lgx:text-[11px] max-lgx:ml-[20px]">
+            <p className="text-gray4  text-center  w-[200px] text-[13px] font-[YekanBakh]  max-lgx:text-[11px] ">
               {" "}
-              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-              استفاده از طراحان گرافیک است{" "}
+              داده‌ها بیش از هر زمان دیگری در زندگی ما نقش بازی می‌کنند
             </p>
           </div>
 
-          <div className="it3  border-[#eeeeee] border-[1px] border-solid max-xl:w-[270px] max-xl:h-[270px] max-lgx:w-[240px] max-lgx:h-[240px]   w-[300px] h-[300px] rounded-3xl   absolute max-xl:right-10 max-xl:top-[370px]  max-lgx:right-36 max-lgx:top-[320px] right-0 top-[370px]  bg-white  ">
-            <div className="icon2  w-[80px]  h-[80px] absolute  top-[50px] inset-x-[110px] max-xl:left-24 max-lgx:left-20 max-lgx:top-[33px]">
+          <div className="it3 flex justify-center items-center py-10 flex-wrap border-[#eeeeee] border-[1px] border-solid max-xl:w-[270px] max-xl:h-[270px] max-lgx:w-[240px] max-lgx:h-[240px]   w-[300px] h-[300px] rounded-3xl   absolute max-xl:right-10 max-xl:top-[370px]  max-lgx:right-36 max-lgx:top-[320px] right-0 top-[370px]  bg-white  ">
+            <div className="icon2  w-[80px]  h-[80px] ">
               <img src={pic8} alt="image" />{" "}
             </div>
-            <h1 className="text-gray3 text-center text-3xl mt-36 text-[24px] font-[YekanBakhBold] leading-[50px]  max-lgx:text-[19px] max-lgx:mt-[120px]">
+            <h1 className="text-gray3 w-full text-center text-3xl text-[24px] font-[YekanBakhBold] leading-[50px]  max-lgx:text-[19px]">
               {" "}
               امنیت شبکه
             </h1>
-            <p className="text-gray4  text-center w-[200px] text-[13px] font-[YekanBakh] ml-[50px] max-xl:ml-[40px]  max-lgx:text-[11px] max-lgx:ml-[20px]">
-              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-              استفاده از طراحان گرافیک است{" "}
+            <p className="text-gray4  text-center w-[200px] text-[13px] font-[YekanBakh] max-lgx:text-[11px] ">
+              امنیت شبکه یکی از مسائل حیاتی در دنیای فناوری اطلاعات است. دوره
+              امنیت شبکه برای تسلط بر تهدیدات و حفاظت از اطلاعات 
+              الزامی است
             </p>
           </div>
 
-          <div className="it4  border-[#eeeeee] border-[1px] border-solid max-xl:w-[270px] max-xl:h-[270px]  max-lgx:w-[240px] max-lgx:h-[240px]  w-[300px] h-[300px] rounded-3xl   absolute  max-xl:top-[420px] max-lgx:top-[370px] left-0 bottom-[10px] bg-white  ">
-            <div className="icon2  w-[80px]  h-[80px] absolute  top-[50px] inset-x-[110px] max-xl:left-24 max-lgx:left-20 max-lgx:top-[33px] ">
+          <div className="it4 flex justify-center items-center py-10 flex-wrap border-[#eeeeee] border-[1px] border-solid max-xl:w-[270px] max-xl:h-[270px]  max-lgx:w-[240px] max-lgx:h-[240px]  w-[300px] h-[300px] rounded-3xl   absolute  max-xl:top-[420px] max-lgx:top-[370px] left-0 bottom-[10px] bg-white  ">
+            <div className="icon2  w-[80px]  h-[80px] ">
               <img src={pic9} alt="image" />{" "}
             </div>
-            <h1 className="text-gray3 text-center text-3xl mt-36 text-[24px] font-[YekanBakhBold] leading-[50px]  max-lgx:text-[19px] max-lgx:mt-[120px]">
+            <h1 className="text-gray3 w-full text-center text-3xl text-[24px] font-[YekanBakhBold] leading-[50px]  max-lgx:text-[19px]">
               {" "}
               وب دیزاین{" "}
             </h1>
-            <p className="text-gray4  text-center w-[200px] text-[13px] font-[YekanBakh] ml-[50px] max-xl:ml-[40px] max-lgx:text-[11px] max-lgx:ml-[20px]">
-              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-              استفاده از طراحان گرافیک است
+            <p className="text-gray4  text-center w-[200px] text-[13px] font-[YekanBakh]max-lgx:text-[11px] ">
+              دوره ی طراحی وبسایت یکی از پر مخاطب ترین دوره طراحی وب سایت
+              است، این دوره هم به صورت آنلاین برگزار می گردد
             </p>
           </div>
         </div>
       </div>
       <div className="w-[95%] max-short:w-[90%] h-[400px] flex justify-evenly flex-wrap gap-6 mt-20 dd:hidden ">
         <div className="w-[47%] max-short:w-[160px] max-short:h-[160px] h-[2/4] flex justify-center flex-nowrap max-sm:flex-wrap hadow-[1px_1px_20px_1px] bg-white shadow-[#fff586] border-[#fff373] border-solid border-[1px] rounded-[16px]">
-        <div className="w-[60%] h-full flex justify-end flex-wrap items-center max-sm:hidden ">
-          <p className="w-full h-[30%] text-gray3 text-end pt-2 text-[20px] ProjectFontBold max-md:text-[18px] max-sm:hidden">بازی</p>
-          <p className="text-gray4 text-right w-[90%] mt-[-90px] text-[13px] max-md:text-[12px] max-sm:hidden">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</p>
-        </div>
-        <div className="w-[40%] h-full flex justify-center flex-nowrap items-center max-sm:w-full max-sm:h-[70%]">
-          <img src={pic6} className="w-[80px] max-short:w-[60px] max-short:h-[60px] h-[80px] "alt="" />
-        </div>
-        <div className="hidden text-center ProjectFontBold max-short:text-[18px] text-[22px] max-sm:block w-full h-[30%]"><p>بازی</p></div>
-        
+          <div className="w-[60%] h-full flex justify-end flex-wrap items-center max-sm:hidden ">
+            <p className="w-full h-[30%] text-gray3 text-end pt-2 text-[20px] ProjectFontBold max-md:text-[18px] max-sm:hidden">
+              بازی
+            </p>
+            <p className="text-gray4 text-right w-[90%] mt-[-90px] text-[13px] max-md:text-[12px] max-sm:hidden">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+              استفاده از طراحان گرافیک است
+            </p>
+          </div>
+          <div className="w-[40%] h-full flex justify-center flex-nowrap items-center max-sm:w-full max-sm:h-[70%]">
+            <img
+              src={pic6}
+              className="w-[80px] max-short:w-[60px] max-short:h-[60px] h-[80px] "
+              alt=""
+            />
+          </div>
+          <div className="hidden text-center ProjectFontBold max-short:text-[18px] text-[22px] max-sm:block w-full h-[30%]">
+            <p>بازی</p>
+          </div>
         </div>
         <div className="w-[47%] max-short:w-[160px] max-short:h-[160px] h-[2/4] flex justify-center flex-nowrap max-sm:flex-wrap shadow-[1px_1px_20px_1px] shadow-[#d8d0fc] border-[#c0b2ff] border-solid border-[1px] rounded-[16px]">
-        <div className="w-[60%] h-full flex justify-end flex-wrap items-center max-sm:hidden">
-          <p className="w-full h-[30%] text-gray3 text-end pt-2 text-[20px] ProjectFontBold max-md:text-[18px] max-sm:hidden">تحیل داده</p>
-          <p className="text-gray4 text-right w-[90%] mt-[-90px] text-[13px] max-md:text-[12px] max-sm:hidden">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</p>
-        </div>
-        <div className="w-[40%] h-full flex justify-center flex-nowrap items-center max-sm:w-full max-sm:h-[70%]">
-          <img src={pic7} className="w-[80px] max-short:w-[60px] max-short:h-[60px] h-[80px] "alt="" />
-        </div>
-        <div className="hidden text-center ProjectFontBold max-short:text-[18px] text-[22px] max-sm:block w-full h-[30%]"><p>تحیل داده</p></div>
-        
+          <div className="w-[60%] h-full flex justify-end flex-wrap items-center max-sm:hidden">
+            <p className="w-full h-[30%] text-gray3 text-end pt-2 text-[20px] ProjectFontBold max-md:text-[18px] max-sm:hidden">
+              تحیل داده
+            </p>
+            <p className="text-gray4 text-right w-[90%] mt-[-90px] text-[13px] max-md:text-[12px] max-sm:hidden">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+              استفاده از طراحان گرافیک است
+            </p>
+          </div>
+          <div className="w-[40%] h-full flex justify-center flex-nowrap items-center max-sm:w-full max-sm:h-[70%]">
+            <img
+              src={pic7}
+              className="w-[80px] max-short:w-[60px] max-short:h-[60px] h-[80px] "
+              alt=""
+            />
+          </div>
+          <div className="hidden text-center ProjectFontBold max-short:text-[18px] text-[22px] max-sm:block w-full h-[30%]">
+            <p>تحیل داده</p>
+          </div>
         </div>
         <div className="w-[47%] max-short:w-[160px] max-short:h-[160px] h-[2/4] flex justify-center flex-nowrap max-sm:flex-wrap shadow-[1px_1px_20px_1px] shadow-[#a9f8cf] border-[#d1fae5] border-solid border-[1px] rounded-[16px]">
-        <div className="w-[60%] h-full flex justify-end flex-wrap items-center max-sm:hidden">
-          <p className="w-full h-[30%] text-gray3 text-end pt-2 text-[20px] ProjectFontBold max-md:text-[18px] ">امنیت شبکه</p>
-          <p className="text-gray4 text-right w-[90%] mt-[-90px] text-[13px] max-md:text-[12px] max-sm:hidden">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</p>
-        </div>
-        <div className="w-[40%] h-full flex justify-center flex-nowrap items-center max-sm:w-full max-sm:h-[70%]">
-          <img src={pic8} className="w-[80px] max-short:w-[60px] max-short:h-[60px] h-[80px] "alt="" />
-        </div>
-        <div className="hidden text-center ProjectFontBold max-short:text-[18px] text-[22px] max-sm:block w-full h-[30%]"><p>امنیت شبکه</p></div>
-        
+          <div className="w-[60%] h-full flex justify-end flex-wrap items-center max-sm:hidden">
+            <p className="w-full h-[30%] text-gray3 text-end pt-2 text-[20px] ProjectFontBold max-md:text-[18px] ">
+              امنیت شبکه
+            </p>
+            <p className="text-gray4 text-right w-[90%] mt-[-90px] text-[13px] max-md:text-[12px] max-sm:hidden">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+              استفاده از طراحان گرافیک است
+            </p>
+          </div>
+          <div className="w-[40%] h-full flex justify-center flex-nowrap items-center max-sm:w-full max-sm:h-[70%]">
+            <img
+              src={pic8}
+              className="w-[80px] max-short:w-[60px] max-short:h-[60px] h-[80px] "
+              alt=""
+            />
+          </div>
+          <div className="hidden text-center ProjectFontBold max-short:text-[18px] text-[22px] max-sm:block w-full h-[30%]">
+            <p>امنیت شبکه</p>
+          </div>
         </div>
         <div className="w-[47%] max-short:w-[160px] max-short:h-[160px] h-[2/4] flex justify-center flex-nowrap max-sm:flex-wrap shadow-[1px_1px_20px_1px] shadow-[#fccde8] border-[#ffbfe3] border-solid border-[1px] rounded-[16px]">
-        <div className="w-[60%] h-full flex justify-end flex-wrap items-center max-sm:hidden">
-          <p className="w-full h-[30%] text-gray3 text-end pt-2 text-[20px] ProjectFontBold max-md:text-[18px] max-sm:hidden">وب دیزاین</p>
-          <p className="text-gray4 text-right w-[90%] mt-[-90px] text-[13px] max-md:text-[12px] ">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</p>
-        </div>
-        <div className="w-[40%] h-full flex justify-center flex-nowrap items-center max-sm:w-full max-sm:h-[70%]">
-          <img src={pic9} className="w-[80px] max-short:w-[60px] max-short:h-[60px] h-[80px] "alt="" />
-        </div>
-        <div className="hidden text-center ProjectFontBold max-short:text-[18px] text-[22px] max-sm:block w-full h-[30%]"><p>وب دیزاین</p></div>
-        
+          <div className="w-[60%] h-full flex justify-end flex-wrap items-center max-sm:hidden">
+            <p className="w-full h-[30%] text-gray3 text-end pt-2 text-[20px] ProjectFontBold max-md:text-[18px] max-sm:hidden">
+              وب دیزاین
+            </p>
+            <p className="text-gray4 text-right w-[90%] mt-[-90px] text-[13px] max-md:text-[12px] ">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+              استفاده از طراحان گرافیک است
+            </p>
+          </div>
+          <div className="w-[40%] h-full flex justify-center flex-nowrap items-center max-sm:w-full max-sm:h-[70%]">
+            <img
+              src={pic9}
+              className="w-[80px] max-short:w-[60px] max-short:h-[60px] h-[80px] "
+              alt=""
+            />
+          </div>
+          <div className="hidden text-center ProjectFontBold max-short:text-[18px] text-[22px] max-sm:block w-full h-[30%]">
+            <p>وب دیزاین</p>
+          </div>
         </div>
       </div>
     </div>
