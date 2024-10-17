48144c72558cb8965f4d0b01031498bd773b787c
barane <baranemirzaei@gmail.com>
2024-10-17 12:59:46 +0330

    edit navbar

--- src/app/Layouts/MainLayout/Navbar/index.jsx
+++ src/app/Layouts/MainLayout/Navbar/index.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
                  </button>             
 
         </div>
-<div className="flex  gap-12 flex-row-reverse justify-around whitespace-nowrap mt-[35px] mx-[21%]  text-[#455a64]  ">
+<div className="flex  gap-12 flex-row-reverse justify-around whitespace-nowrap mt-[37px] mx-[21%]  text-[#455a64]  ">
 
 
         {/* <div className="flex flex-row-reverse    w-[40%]  mt-[30px]  text-[#455a64]"> */}
