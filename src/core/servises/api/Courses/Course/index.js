1c30a70f564b3ad7d966a1fbd198ba651134d862
METi <mahdik1385427@gmail.com>
2024-10-18 09:50:51 +0330

    9 item

--- src/core/servises/api/Courses/Course/index.js
+++ src/core/servises/api/Courses/Course/index.js
@@ -1,12 +1,12 @@
 import http from "../../../interceptor/Interseptor";
 
-export const getCourseList = async (count) => {
+export const getCourseList = async (params) => {
   try {
     console.log("fetching started...");
 
     const result = await http.get(
-      "/Home/GetCoursesWithPagination?PageNumber=1&RowsOfPage=10&SortingCol=Active&SortType=DESC&Tech",
-      { params: count }
+      "/Home/GetCoursesWithPagination?PageNumber=2&SortingCol=Active&SortType=DESC&TechCount=0",
+      { params: params }
     );
 
     return result;
