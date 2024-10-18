e879348886d4b38dfcb853b6e73661f6eccd0fd4
barane <baranemirzaei@gmail.com>
2024-10-19 00:32:36 +0330

    blog

--- src/components/Landing/Blog/TopBlog/index.jsx
+++ src/components/Landing/Blog/TopBlog/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import view from "@assets/images/Landing/Blogs/view.png";
+import noImage from "@assets/images/Landing/Blogs/noImage.jpg";
+
 const TopBlog = ({
   title,
   miniDescribe,
@@ -8,12 +10,21 @@ const TopBlog = ({
   currentImageAddressTumb,
 }) => {
   return (
-    <Link
-      to="/BlogDetailsS"
-      className=" w-[597px] h-[570px] mt-[-70px] flex justify-end flex-wrap max-xl:hidden"
+    <div className=' w-[597px] h-[570px] mt-[-70px] flex justify-end flex-wrap max-xl:hidden'
     >
-      <div className="laptop w-full h-[60%] rounded-[24px] bg-no-repeat bg-right relative">
-        <h2 className="absolute bottom-[-110px] right-0">{title}</h2>
+        <Link>
+       
+
+        </Link>
+        
+      <div className="laptop w-[90%] h-[60%] rounded-[24px] bg-no-repeat bg-right relative border">
+         <div className="   w-[100%] h-[100%]   float-left">  <img
+            className="rounded-[24px]"
+            src={currentImageAddressTumb ? currentImageAddressTumb : noImage}
+            alt=""
+          /></div>
+      <h2 className="absolute bottom-[-110px] right-0">{title}</h2>
+
       </div>
       <div className=" w-[97%] h-[38%] flex justify-end flex-wrap pt-4">
         <div className="date w-[100px] h-[40px] bg-[#DAEEFF] rounded-[80px] bg-no-repeat bg-[center_right_1rem] text-right text-[13px] font-bold text-[#2196F3] pr-9 pt-[13px]">
@@ -29,7 +40,7 @@ const TopBlog = ({
           {miniDescribe}
         </h4>
       </div>
-    </Link>
+    </div>
   );
 };
 
