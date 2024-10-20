fe506ccc7c5d59329c8af0030047abd95fd6c92c
barane <baranemirzaei@gmail.com>
2024-10-21 01:06:46 +0330

    fix cm

--- src/components/CourseDetail/TabBox/Comment/AddReply/Content/index.jsx
+++ src/components/CourseDetail/TabBox/Comment/AddReply/Content/index.jsx
@@ -1,39 +1,39 @@
-import React from 'react'
-import { Form, Formik,Field } from "formik";
+// import React from 'react'
+// import { Form, Formik,Field } from "formik";
 
 
-const Content = () => {
-  return (
-    <div className="commentFormSection">
-      <Formik
+// const Content = () => {
+//   return (
+//     <div className="commentFormSection">
+//       <Formik
        
-      >
-        <Form className="w-full">
-          <div className="w-full">
-            <div className="relative">
+//       >
+//         <Form className="w-full">
+//           <div className="w-full">
+//             <div className="relative">
             
-              <div className=" text-start  border border-[#cdd5de] rounded-lg  shadow-md">
-                <Field dir="rtl"
-                  as="textarea"
-                  id="describe"
-                  name="describe"
-                  className="commentFormTextarea w-[500px] h-[100px] "
-                  placeholder="نظر خودتو بنویس..."
-                />
+//               <div className=" text-start  border border-[#cdd5de] rounded-lg  shadow-md">
+//                 <Field dir="rtl"
+//                   as="textarea"
+//                   id="describe"
+//                   name="describe"
+//                   className="commentFormTextarea w-[500px] h-[100px] "
+//                   placeholder="نظر خودتو بنویس..."
+//                 />
                
-              </div>
-            </div>
-          </div>
-          {/* <div className="w-[60px] h-[40px] bg-bluee mx-[50%] mt-[20%] rounded-[50px]  ">
-                <span className="font-[YekanBakh] relative top-2 left-3.5 text-[15px] text-white mx-auto  ">
-                  {" "}
-                  ارسال{" "}
-                </span>
-              </div> */}
-        </Form>
-      </Formik>
-    </div>
-  )
-}
+//               </div>
+//             </div>
+//           </div>
+//           {/* <div className="w-[60px] h-[40px] bg-bluee mx-[50%] mt-[20%] rounded-[50px]  ">
+//                 <span className="font-[YekanBakh] relative top-2 left-3.5 text-[15px] text-white mx-auto  ">
+//                   {" "}
+//                   ارسال{" "}
+//                 </span>
+//               </div> */}
+//         </Form>
+//       </Formik>
+//     </div>
+//   )
+// }
 
-export default Content
+// export default Content
