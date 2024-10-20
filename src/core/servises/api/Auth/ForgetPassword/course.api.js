847869ed9368a5ef9b7d11d5f44d9d2e5056010f
barane <baranemirzaei@gmail.com>
2024-10-20 20:04:24 +0330

    fixing landing

--- src/core/servises/api/Auth/ForgetPassword/course.api.js
+++ src/core/servises/api/Auth/ForgetPassword/course.api.js
@@ -1,1 +1,35 @@
-import http from "@core/"
+import http from "@core/servises/interceptor/Interseptor"
+
+export const forgetPass = async (obj) => {
+    try {
+      const response = await http.post(`/Sign/ForgetPassword`, obj);
+  
+      return response;
+    } catch (error) {
+      return false;
+    }
+  };
+
+
+
+  export const resetConfirm = async (ConfigValue) => {
+    try {
+      //console.log("Fetching started...");
+      const result = await http.get(`Sign/Reset/${ConfigValue}`);
+  
+      return result;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  };
+
+  export const resetPass = async (value) => {
+    try {
+      const response = await http.post("/Sign/Reset", value);
+  
+      return response;
+    } catch (error) {
+      return false;
+    }
+  };
