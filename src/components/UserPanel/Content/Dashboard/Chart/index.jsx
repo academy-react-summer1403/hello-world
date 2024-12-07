46db0fab76eeeb34966e7f1e212cca29e95e92f2
nima <nimaruhi85@gmail.com>
2024-12-07 13:22:24 +0330

    bog fix

--- src/components/UserPanel/Content/Dashboard/Chart/index.jsx
+++ src/components/UserPanel/Content/Dashboard/Chart/index.jsx
@@ -3,7 +3,10 @@ import { PieChart } from '@mui/x-charts/PieChart';
 
 export default function BasicPie() {
   return (
-<div className='relative top-[-85px] left-[280px]'>
+<div className='relative top-[-85px] left-[280px] border-[3px] border-[#d5d4fe] rounded-[20px]'>
+<div className='absolute left-[225px]'>
+    <p>تعداد د وره ها و اخبار مقالات</p>
+    </div>
     <PieChart
       series={[
         {
